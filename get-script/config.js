e84d689e93bdd9c597f1b1f7460796611248bf46
aiyoudiao <newdiao@163.com>
2022-12-18 16:21:14 +0800

    feat: update holidays.json.

--- get-script/config.js
+++ get-script/config.js
@@ -3,7 +3,7 @@ const date = new Date();
 
 exports.startYear = date.getFullYear();
 exports.endYear = date.getFullYear() + 2;
-exports.startMonth = date.getMonth() + 2;
+exports.startMonth = date.getMonth() + 1;
 exports.endMonth = 1;
 
 exports.festivalJsonFilePath = path.resolve("./data/zh-cn/festival.json");
